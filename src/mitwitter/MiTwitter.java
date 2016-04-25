0dc4b82497dcfbcfc2c802f3f8275b89908c69f6
mmoureperez <mmoureperez@atanasoff02.danielcastelao.org>
2016-04-25 09:22:01 +0200

    modificacion

--- src/mitwitter/MiTwitter.java
+++ src/mitwitter/MiTwitter.java
@@ -6,9 +6,14 @@
 
 package mitwitter;
 
+import java.util.List;
+import twitter4j.Query;
+import twitter4j.QueryResult;
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
+
 
 /**
  *
@@ -19,17 +24,34 @@ public class MiTwitter {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TwitterException {
         // TODO code application logic here
-        Twitter miTwitter= new TwitterFactory().getInstance();
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-    cb.setDebugEnabled(true)
-  .setOAuthConsumerKey("*********************")
-  .setOAuthConsumerSecret("******************************************")
-  .setOAuthAccessToken("**************************************************")
-  .setOAuthAccessTokenSecret("******************************************");
-TwitterFactory tf = new TwitterFactory(cb.build());
-Twitter twitter = tf.getInstance();
+        
+       // ConfigurationBuilder cb = new ConfigurationBuilder();
+        //La configurationBuilder paso a un fichero. 
+      
+        
+        Twitter twitter = new TwitterFactory().getInstance();
+        
+        //CODIGO CAMBIAR ESTADO
+        Status miStatus = twitter.updateStatus("Tercer ejemplo");//cambia el estado de twitter
+        System.out.println(miStatus.getText());
+
+        //CODIGO TIMELINE
+        List<Status> statuses = twitter.getHomeTimeline();
+        System.out.println("Showing home timeline.");//aparecen los ultimos twitts
+        for (Status status : statuses) {
+            System.out.println(status.getUser().getName() + ":" + status.getText());
+        }
+
+        //CODIGO BUSCAR TAGS
+        Query query = new Query("Chelsea"); //Dentro del String va el tag que quieres buscar
+        QueryResult result = twitter.search(query);
+        for (Status status : result.getTweets()) {
+            System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+        }
+        
+
     }
     
 }
